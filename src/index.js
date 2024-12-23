9df9ff31311c6b914ad4b33ccde71ee1df1a23be
bluwy <bjornlu.dev@gmail.com>
2024-12-23 18:09:37 +0100

    Improve section UI

--- src/index.js
+++ src/index.js
@@ -22,8 +22,9 @@ async function run() {
 
 async function repoHasChangesetsSetup() {
   const orgRepo = window.location.pathname.split('/').slice(1, 3).join('/')
+  // ideally `.base-ref` should also be used, but it's not added when a pr is closed/merged
   const baseBranch = document
-    .querySelector('.commit-ref.base-ref')
+    .querySelector('.commit-ref')
     .title.split(':')[1]
     .trim()
 
@@ -94,45 +95,41 @@ async function addChangesetSideSection(updatedPackages) {
   })}.md`
   const changesetFileContent = `\
 ---
-pkg: patch
+package: patch
 ---
 
 ${prTitle}
 `
 
-  const canEditPr = document.querySelector('button.js-title-edit-button')
+  const canEditPr = !!document.querySelector('button.js-title-edit-button')
+  const isPrOpen = !!document.querySelector('.State.State--open')
   const notificationsSideSection = document.querySelector(
     '.discussion-sidebar-item.sidebar-notifications'
   )
-
   const plusIcon = `<svg class="octicon octicon-plus" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path d="M7.75 2a.75.75 0 0 1 .75.75V7h4.25a.75.75 0 0 1 0 1.5H8.5v4.25a.75.75 0 0 1-1.5 0V8.5H2.75a.75.75 0 0 1 0-1.5H7V2.75A.75.75 0 0 1 7.75 2Z"></path></svg>`
 
   // add new section just before the notifications section
-  const html = `\
-${
-  canEditPr
-    ? `<a class="d-block text-bold discussion-sidebar-heading discussion-sidebar-toggle" href="https://github.com/${orgRepo}/new/${branch}?filename=${changesetFileName}&value=${encodeURIComponent(
-        changesetFileContent
-      )}">Changesets\n${plusIcon}</a>`
-    : `<div class="d-block text-bold discussion-sidebar-heading">Changesets</div>`
-}
-${
-  Object.keys(updatedPackages).length
-    ? `\
-<table style="width: 100%">
+  let html =
+    canEditPr && isPrOpen
+      ? `<a class="d-block text-bold discussion-sidebar-heading discussion-sidebar-toggle" href="https://github.com/${orgRepo}/new/${branch}?filename=${changesetFileName}&value=${encodeURIComponent(
+          changesetFileContent
+        )}">Changesets\n${plusIcon}</a>`
+      : `<div class="d-block text-bold discussion-sidebar-heading">Changesets</div>`
+  if (Object.keys(updatedPackages).length) {
+    html += `\
+<table style="width: 100%; max-width: 400px;">
   <tbody>
     ${Object.entries(updatedPackages)
       .map(
         ([pkg, bumps]) =>
-          `<tr><td>${pkg}</td><td class="color-fg-muted">${bumps.join(
+          `<tr><td style="width: 1px; white-space: nowrap; padding-right: 8px;">${pkg}</td><td class="color-fg-muted">${bumps.join(
             ', '
           )}</td></tr>`
       )
       .join('')}
   </tbody>  
 </table>`
-    : ''
-}`
+  }
 
   const changesetSideSection = document.createElement('div')
   changesetSideSection.className = 'discussion-sidebar-item sidebar-changesets'
@@ -156,7 +153,6 @@ function getUpdatedPackagesFromAddedChangedFiles(changedFiles) {
   for (const file of changedFiles) {
     if (file.filename.startsWith('.changeset/') && file.status === 'added') {
       const yaml = /---.+---/s.exec(file.patch)?.[0]
-      console.log(file.patch, yaml)
       if (!yaml) continue
       const matched = yaml.matchAll(/\+(.+?):\s*(major|minor|patch)\n/g)
       for (const match of matched) {
